913170ad5cb70c8f30f0c38455d0f1bda47e2b6a
unknown <imran nazir>
2022-05-30 06:49:45 +0600

    navbar designed

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -3,8 +3,26 @@ import TopBar from "./Topbar";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="bg-neutral">
       <TopBar />
+      <nav className="flex justify-between items-center text-secondary px-[5%]">
+        <div className="flex gap-6 uppercase">
+          <a href="" className="text-primary">
+            Home
+          </a>
+          <a href="">Advertisers</a>
+          <a href="">Publishers</a>
+          <a href="">Influencers</a>
+          <a href="">Blog</a>
+          <a href="">Contact us</a>
+        </div>
+        <div className="bg-base-100 rounded-full px-4 py-2 flex gap-2 shadow-lg">
+          <button className="btn button btn-ghost rounded-full">Login</button>
+          <button className="btn btn-primary rounded-full text-white">
+            Sign Up
+          </button>
+        </div>
+      </nav>
     </div>
   );
 };
--- src/Pages/Home/Topbar.jsx
+++ src/Pages/Home/Topbar.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 const TopBar = () => {
   return (
-    <div className="flex justify-between px-[5%]">
+    <div className="flex justify-between bg-neutral px-[5%]">
       <div>
-        <p>LOGO</p>
+        <p className="text-3xl font-bold py-2">LOGO</p>
       </div>
       <div>
-        <button>En</button>/<button>Bn</button>
+        <button className="btn btn-link text-success">Bn</button>/
+        <button className="btn btn-link text-black">En</button>
       </div>
     </div>
   );
