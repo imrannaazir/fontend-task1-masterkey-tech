1043d60920eaa27cc802b50333961eba14cde826
unknown <imran nazir>
2022-05-30 15:45:34 +0600

    slider updated

--- src/Pages/Home/Sliders.jsx
+++ src/Pages/Home/Sliders.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -8,9 +8,10 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 import "../Home/Styles/styles.css";
+import "../Home/Styles/Swiper.css";
 
 // import required modules
-import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+import { Navigation, Pagination } from "swiper";
 import Slide from "./Slide";
 
 const Sliders = () => {
@@ -20,17 +21,23 @@ const Sliders = () => {
       .then((res) => res.json())
       .then((data) => setSliders(data));
   }, []);
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
   return (
     <div className="bg-neutral shadow-2xl">
       <h1 className="text-secondary text-4xl text-center py-16">Title 1</h1>
       <Swiper
         cssMode={true}
         navigation={true}
+        onInit={(swiper) => {
+          swiper.params.navigation.prevEl = prevRef.current;
+          swiper.params.navigation.nextEl = nextRef.current;
+          swiper.navigation.init();
+          swiper.navigation.update();
+        }}
         pagination={true}
-        mousewheel={true}
-        keyboard={true}
         slidesPerView={1.8}
-        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        modules={[Navigation, Pagination]}
         className="mySwiper"
       >
         {sliders.map((slider) => (
@@ -38,6 +45,21 @@ const Sliders = () => {
             <Slide key={slider.id} slider={slider} />
           </SwiperSlide>
         ))}
+        <div className="flex justify-center gap-12 mb-16">
+          <div
+            className="cursor-pointer bg-white rounded-full w-14 h-14 text-4xl text-secondary flex justify-center items-center"
+            ref={prevRef}
+          >
+            {"<"}
+          </div>
+          <br />
+          <span
+            className="cursor-pointer bg-white rounded-full w-14 h-14 text-4xl text-secondary flex justify-center items-center"
+            ref={nextRef}
+          >
+            {">"}
+          </span>
+        </div>
       </Swiper>
     </div>
   );
